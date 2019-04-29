e172ea0320577ec1fe8ec2a3aca32c315b104003
Gayan Sampath Manamendra <33378546+GayanSampathManamendra@users.noreply.github.com>
2019-04-29 23:00:02 +0530

    file set updated

---
+++ src/main/java/com/clientBackend/controller/loginAndSignup.java
@@ -0,0 +1,17 @@
+package com.clientBackend.controller;
+
+public class loginAndSignup {
+	
+	// request mapping 
+	// ../customer/signup/signupAuthentication
+	// public signupAuthentication(){
+			// get the id of purticular object and push into the session.
+	//}
+	
+	// ../customer/login/loginAuthentication
+	// public loginAuthentication(){
+			// authentication of the login and get the id of the customer and put into the session
+	//}
+	
+
+}
---
+++ src/main/java/com/clientBackend/controller/trasherCommentsController.java
@@ -0,0 +1,15 @@
+package com.clientBackend.controller;
+
+public class trasherCommentsController {
+	
+	// requestMapping urls
+	
+	// ../customer/smallTrasher/comment
+	
+	// ../customer/primumTrasher/comment
+	
+	// ../customer/largeTrasher/comment
+	
+	// method for return all the comments for purticular trasher
+
+}
